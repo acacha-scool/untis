cb6973fb6e7fec04865aa426a0bea6a3e5482ff7
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-09-10 20:21:41 +0200

    Sync

--- src/SkeletonClass.php
+++ src/SkeletonClass.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace League\Skeleton;
+namespace Scool\Untis;
 
 class SkeletonClass
 {
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace League\Skeleton;
+namespace Scool\Untis;
 
 class ExampleTest extends \PHPUnit\Framework\TestCase
 {
